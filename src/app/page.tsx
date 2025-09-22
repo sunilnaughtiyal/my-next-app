251708af31d9362007fa9fc1fb548247e28093e4
SUNIL\sunil <sunil.sharma1@tothenew.com>
2025-09-22 17:14:24 +0530

    log updated test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export const dynamic = "force-dynamic";
 
 export default function Home() {
-  console.log("📢 Home page rendered on server at:", new Date().toISOString());
+  console.log("📢 Home page rendered on the server at:", new Date().toISOString());
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
