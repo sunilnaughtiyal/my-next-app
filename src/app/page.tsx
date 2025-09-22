118f14fea697ac4f4bb32cecad8d3e1af8616085
SUNIL\sunil <sunil.sharma1@tothenew.com>
2025-09-22 15:05:05 +0530

    log updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
+  // ✅ Ye log Vercel Logs me dikhega jab bhi page server-side render hoga
+  console.log("📢 Home page rendered at:", new Date().toISOString());
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
